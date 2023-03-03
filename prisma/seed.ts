e63fb8d37fba9d7053e32c9d41521d076d04b8a1
Luciano <luciano.rangel.aguiar@gmail.com>
2023-03-03 13:42:39 -0300

    seed updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   let event = await prisma.event.findFirst();
+  let tickets = await prisma.ticketType.findMany();
+
   if (!event) {
     event = await prisma.event.create({
       data: {
@@ -17,6 +19,30 @@ async function main() {
   }
 
   console.log({ event });
+
+  if (tickets.length < 2) {
+    await prisma.ticketType.deleteMany({})
+    await prisma.ticketType.create({
+      data: {
+        name: "Presencial",
+        price: 250,
+        isRemote: false,
+        includesHotel: true,
+        updatedAt: dayjs().toDate(),
+      },
+    })
+    await prisma.ticketType.create({
+      data: {
+        name: "Online",
+        price: 100,
+        isRemote: true,
+        includesHotel: false,
+        updatedAt: dayjs().toDate(),
+      },
+    })
+    tickets = await prisma.ticketType.findMany();
+  }
+  console.log({ tickets });
 }
 
 main()
