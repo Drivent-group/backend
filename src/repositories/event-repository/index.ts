385a1c5d88169897f6d657143130907e2dd711d5
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-03-08 22:35:30 -0300

    feat: add Redis for faster access in event route

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,17 @@
-import { prisma } from "@/config";
+import { prisma, redis } from "@/config";
+import { Event } from "@prisma/client/index";
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cacheKey = "event";
+
+  const event: Event = JSON.parse(await redis.get(cacheKey));
+
+  if (event) return event;
+  else {
+    const data = await prisma.event.findFirst();
+    redis.setEx(cacheKey, parseInt(process.env.EXPIRATION), JSON.stringify(data));
+    return data;
+  }
 }
 
 const eventRepository = {
